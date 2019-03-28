83c8872430c3c1589fb31f04b935063e3a9c4366
hamger <2313303800@qq.com>
2019-03-28 21:12:57 +0800

    change index-test

--- src/__tests__/index-test.js
+++ src/__tests__/index-test.js
@@ -1,9 +1,11 @@
 /* eslint-env jest */
-import Block from '../index';
-import BlockComponent from '../components/Block';
+import { Slot, withSlot } from '../index';
+import SlotComponent from '../components/Slot';
+import withSlotFunc from '../components/withSlot';
 
 describe('index.js', () => {
   it('should correctly export all component as named export', () => {
-    expect(Block).toEqual(BlockComponent);
+    expect(Slot).toEqual(SlotComponent);
+    expect(withSlot).toEqual(withSlotFunc);
   });
 });
